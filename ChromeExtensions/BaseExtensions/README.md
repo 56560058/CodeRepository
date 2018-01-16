db32843baf382515e4de856259798b0f0a3921d0
56560058 <56560058@qq.com>
2018-01-16 14:30:43 +0800

    J2EE exam 提交

---
+++ ChromeExtensions/BaseExtensions/README.md
@@ -0,0 +1,3 @@
+# Chrome
+
+https://jingyan.baidu.com/article/ce09321b7d581e2bff858f23.html
---
+++ ChromeExtensions/BaseExtensions/js/inject.js
@@ -0,0 +1,12 @@
+﻿//inject方法
+function inject(){
+    setBelieveWeb();
+
+    console.log($("#userid").value);
+
+}
+//console
+console.log("inject.js在工作！")
+inject();
+
+
